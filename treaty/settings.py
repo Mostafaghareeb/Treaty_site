3fd0d4866ed6554ebf6ac087d7c3295e79ac258a
Mostafa Ghareeb <sasaghareebmmi@gmail.com>
2025-05-03 07:40:02 +0200

    Back to sqlite3

--- treaty/settings.py
+++ treaty/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = 'treaty.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'treatydb',
-        'USER': 'mostafa',
-        'PASSWORD': 'Sasa1234',
-        'HOST': 'mostafa.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',  
+        'NAME': BASE_DIR / 'db.sqlite3',         
     }
 }
 
